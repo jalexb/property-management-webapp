85ece90e5a9fd8266469422e1eb74e251c277833
Braydon Griffith <braydon0898@gmail.com>
2020-12-05 14:28:06 -0500

    integrate vuetify and begin homepage header

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,9 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="#fadadd" dark>
+      <div class="d-flex align-center">
+
+        <v-btn href="/" 
+        text
+        
+        >
+          <v-avatar size='125'>
+            <img src="..\src\assets\CapstoneLogo.png" alt="logo"/>
+            </v-avatar>        
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="/login"
+        target="_blank"
+        outlined
+        raised 
+        rounded
+        color="#87ceeb"
+        
+      >
+        Login
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
