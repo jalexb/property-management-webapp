003841315977161384b95769d994a644cfe91538
Braydon Griffith <braydon0898@gmail.com>
2020-12-05 10:49:12 -0500

    created intial database template file

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -24,8 +24,44 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
---populate default data
+CREATE TABLE address_table(
+
+	address_id int IDENTITY(1,1) NOT NULL,
+	property_type varchar(50) NOT NULL,
+	street varchar(50) NOT NULL,
+	street2 varchar(50),
+	city varchar(50) NOT NULL,
+	region varchar(50) NOT NULL,
+	zip int NOT NULL,
+	CONSTRAINT PK_address_id PRIMARY KEY (address_id),
+
+)
+
+CREATE TABLE properties (
+
+	property_id int IDENTITY(1,1) NOT NULL,
+	address_id int NOT NULL,
+	user_id int NOT NULL,
+	bedrooms int NOT NULL,
+	bathrooms int NOT NULL,
+	photo varchar(200),
+	prop_description varchar(500) NOT NULL,
+	price decimal NOT NULL,
+	CONSTRAINT PK_property_id PRIMARY KEY (property_id),
+	CONSTRAINT FK_address_id FOREIGN KEY (address_id) REFERENCES address_table (address_id)
+)
+
+
+--populate default user data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
+--populate default address data
+INSERT INTO address_table(property_type, street, street2, city, region, zip) VALUES ('House', '42 Wallaby Way', null, 'Syndey', 'North Carolina', 27009);
+INSERT INTO address_table(property_type, street, street2, city, region, zip) VALUES ('Apt', '500 Smith Rd', 'Building #3 Apt 12', 'San Francisco', 'California', 94016);
+
+--populate default property data
+INSERT INTO properties (address_id, user_id, bedrooms, bathrooms, photo, prop_description, price) VALUES (1, 1, 3, 2, 'https://photos.google.com/photo/AF1QipNFOOdX72uzUqBmi9ekgSQ0Yen_W4XZqISpHaQb', 'Lovely home just a few minutes away from the shore. With three bedrooms and two bathrooms it is sure to have plenty of space and amenities.', 2200);
+INSERT INTO properties (address_id, user_id, bedrooms, bathrooms, photo, prop_description, price) VALUES (2, 2, 2, 1, 'https://photos.google.com/photo/AF1QipMZBmYLv_JuARMkiL8kYIXsjwT4DMfLZflfhe0o', 'Nice apartment with good view and spacing. Perfect for a couple or bachelor.', 1300);
 
 GO
