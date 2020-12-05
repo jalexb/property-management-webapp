ea28b37990c1c8f24cf030b8d48eb793c48b7df7
Amy Scott <amy.ack.scott@gmail.com>
2020-12-05 15:48:17 -0500

    updated sql

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -16,17 +16,17 @@ GO
 
 --create tables
 CREATE TABLE users (
-	user_id			INT IDENTITY(1,1)	NOT NULL,
+	userId			INT IDENTITY(1,1)	NOT NULL,
 	username		VARCHAR(50)			NOT NULL,
 	password_hash	VARCHAR(200)		NOT NULL,
 	salt			VARCHAR(200)		NOT NULL,
 	user_role		VARCHAR(50)			NOT NULL
-	CONSTRAINT		PK_user				PRIMARY KEY (user_id)
-)
+	CONSTRAINT		PK_user				PRIMARY KEY (userId)
+);
 
 CREATE TABLE address_table(
 	address_id		INT IDENTITY(1,1)	NOT NULL,
-	user_id			INT					NOT NULL,
+	userId			INT					NOT NULL,
 	property_type	VARCHAR(50)			NOT NULL,
 	street			VARCHAR(50)			NOT NULL,
 	street2			VARCHAR(50),
@@ -34,35 +34,36 @@ CREATE TABLE address_table(
 	region			VARCHAR(50)			NOT NULL,
 	zip				INT					NOT NULL,
 	CONSTRAINT		PK_address_id		PRIMARY KEY (address_id),
-	CONSTRAINT		FK_user_id			FOREIGN KEY (user_id)		REFERENCES users (user_id)	
-)
+	CONSTRAINT		FK_userId_users		FOREIGN KEY (userId)		REFERENCES users (userId)	
+);
 
 CREATE TABLE properties (
 	property_id		INT IDENTITY(1,1)	NOT NULL,
 	address_id		INT					NOT NULL,
-	user_id			INT					NOT NULL,
+	userId			INT					NOT NULL,
 	bedrooms		INT					NOT NULL,
 	bathrooms		INT					NOT NULL,
 	photo			VARCHAR(200),
 	prop_desc		VARCHAR(500)		NOT NULL,
 	price			DECIMAL				NOT NULL,
-	CONSTRAINT		PK_property_id		PRIMARY KEY (property_id)
-)
+	CONSTRAINT		PK_propertyId		PRIMARY KEY (property_id)
+);
 
 CREATE TABLE lease (
 	lease_id		INT IDENTITY(1,1)	NOT NULL,
 	from_date		DATE,
 	to_date			DATE,
-	user_id			INT					NOT NULL,
+	userId			INT					NOT NULL,
 	property_id		INT					NOT NULL,
 	CONSTRAINT		PK_lease_id			PRIMARY KEY (lease_id),
-	CONSTRAINT		FK_user_id			FOREIGN KEY (user_id)		REFERENCES users (user_id),
-	CONSTRAINT		FK_property_id		FOREIGN KEY (property_id)	REFERENCES properties (property_id)
+	CONSTRAINT		FK_userId_user		FOREIGN KEY (userId)		REFERENCES users (userId),
+	CONSTRAINT		FK_propertyId_prop	FOREIGN KEY (property_id)	REFERENCES properties (property_id)
 	);
 
-
 ALTER TABLE properties
-	ADD			FOREIGN KEY (address_id)	REFERENCES address_table (address_id);
+	ADD	CONSTRAINT	FK_addr_id	
+	FOREIGN KEY (address_id)			
+	REFERENCES address_table (address_id);
 
 
 --populate default user data
@@ -78,21 +79,21 @@ VALUES
 
 --populate default address data
 INSERT INTO address_table
-	(user_id, property_type, street, street2, city, region, zip) 
+	(userId, property_type, street, street2, city, region, zip) 
 VALUES 
 	(1, 'House', '42 Wallaby Way', null, 'Syndey', 'North Carolina', 27009);
 INSERT INTO address_table
-	(user_id, property_type, street, street2, city, region, zip) 
+	(userId, property_type, street, street2, city, region, zip) 
 VALUES 
 	(2, 'Apt', '500 Smith Rd', 'Building #3 Apt 12', 'San Francisco', 'California', 94016);
 
 --populate default property data
 INSERT INTO properties 
-	(address_id, user_id, bedrooms, bathrooms, photo, prop_description, price) 
+	(address_id, userId, bedrooms, bathrooms, photo, prop_desc, price) 
 VALUES 
 	(1, 1, 3, 2, 'https://photos.google.com/photo/AF1QipNFOOdX72uzUqBmi9ekgSQ0Yen_W4XZqISpHaQb', 'Lovely home just a few minutes away from the shore. With three bedrooms and two bathrooms it is sure to have plenty of space and amenities.', 2200);
 INSERT INTO properties 
-	(address_id, user_id, bedrooms, bathrooms, photo, prop_description, price) 
+	(address_id, userId, bedrooms, bathrooms, photo, prop_desc, price) 
 VALUES 
 	(2, 2, 2, 1, 'https://photos.google.com/photo/AF1QipMZBmYLv_JuARMkiL8kYIXsjwT4DMfLZflfhe0o', 'Nice apartment with good view and spacing. Perfect for a couple or bachelor.', 1300);
 
