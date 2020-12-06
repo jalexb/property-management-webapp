be8364c9a59cf5d51a86b02aaee6be173c0525c2
jacob barnett <jabarnett97@gmail.com>
2020-12-06 17:39:48 -0500

    App.vue added router-view. Components + NavBar & PageFooter

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,73 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar app color="#fadadd" dark>
-      <div class="d-flex align-center">
-
-        <v-btn href="/" 
-        text
-        
-        >
-          <v-avatar size='125'>
-            <img src="..\src\assets\CapstoneLogo.png" alt="logo"/>
-            </v-avatar>        
-        </v-btn>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="/login"
-        target="_blank"
-        outlined
-        raised 
-        rounded
-        color="#87ceeb"
-        
-      >
-        Login
-      </v-btn>
-    </v-app-bar>
-
+    <nav-bar />
     <v-main>
-      <Home />
+      <router-view />
     </v-main>
-
-    <v-footer
-    color="primary lighten-1"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      <v-btn
-        v-for="link in links"
-        :key="link"
-        color="white"
-        text
-        rounded
-        class="my-2"
-      >
-        {{ link }}
-      </v-btn>
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
+    <page-footer />
+    
 
   </v-app>
 </template>
 
 <script>
+import PageFooter from './components/PageFooter.vue';
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: "App",
 
   components: {
+    NavBar,
+    PageFooter
     
   },
 
---
+++ final-capstone/vue/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar app color="#fadadd" dark>
+      <div class="d-flex align-center">
+        
+        <router-link :to="{name: 'home'}">
+          <v-btn text>
+            <v-avatar size='125'>
+              <img src="@/assets/CapstoneLogo.png" alt="logo"/>
+            </v-avatar>        
+          </v-btn>
+        </router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <router-link v-bind:to="{name: 'login'}">
+        <v-btn
+          outlined
+          raised 
+          rounded
+          color="#87ceeb"
+          >
+          Login
+        </v-btn>
+      </router-link>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "nav-bar",
+
+    data() {
+      return {}
+    }
+
+}
+</script>
+
+<style>
+
+</style>
