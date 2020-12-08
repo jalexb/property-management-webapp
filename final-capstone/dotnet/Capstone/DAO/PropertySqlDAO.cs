a450c68025197bbfae3d863a4a70dd4fc3d00929
Evan Gemmer <evangemmer@gmail.com>
2020-12-08 14:15:18 -0500

    Property details are now properly showing up when you click the link on the list of properties

--- final-capstone/dotnet/Capstone/DAO/PropertySqlDAO.cs
+++ final-capstone/dotnet/Capstone/DAO/PropertySqlDAO.cs
@@ -18,9 +18,9 @@ namespace Capstone.DAO
         /// <summary>
         /// Gets all properties from the properties table and returns all properties
         /// </summary>
-        public List<PropertyAndAddress> getProperty()
+        public List<PropertyAndAddress> getProperties()
         {
-            List<PropertyAndAddress> property = new List<PropertyAndAddress>();
+            List<PropertyAndAddress> properties = new List<PropertyAndAddress>();
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
@@ -33,34 +33,7 @@ namespace Capstone.DAO
 
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    while(reader.Read())
-                    {
-                        PropertyAndAddress prop = new PropertyAndAddress();
-
-                        prop.propertyId = (int)reader["property_id"];
-                        prop.addressId = (int)reader["address_id"];
-                        prop.userId = (int)reader["userId"];
-                        prop.Bedrooms = (int)reader["bedrooms"];
-                        prop.Bathrooms = (int)reader["bathrooms"];
-                        prop.Photo = (string)reader["photo"];
-                        prop.Description = (string)reader["prop_desc"];
-                        prop.Price = (decimal)reader["price"];
-                        prop.Property_Type = (string)reader["property_type"];
-                        prop.Street = (string)reader["street"];
-                        prop.city = (string)reader["city"];
-                        prop.region = (string)reader["region"];
-                        prop.zip = (int)reader["zip"];
-                        if((string)reader["street2"] != "N/A")
-                        {
-                            prop.Street2 = (string)reader["street2"];
-                        }
-                        else
-                        {
-                            prop.Street2 = "";
-                        }
-
-                        property.Add(prop);
-                    }
+                    properties = ConvertToPropertyAndAddress(reader);
 
                 }
             }
@@ -69,11 +42,75 @@ namespace Capstone.DAO
                 Console.WriteLine(e);
             }
 
+            return properties;
+        }
 
+        public PropertyAndAddress GetProperty(int id)
+        {
+            PropertyAndAddress property = new PropertyAndAddress();
+            List<PropertyAndAddress> properties = new List<PropertyAndAddress>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT property_id, properties.address_id, properties.userId, bedrooms, bathrooms, photo, prop_desc, price, " +
+                                                    "property_type, street, street2, city, region, zip " +
+                                                    "FROM properties " +
+                                                    "INNER JOIN address_table ON properties.address_id = address_table.address_id " +
+                                                    "WHERE property_id = @id", conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    properties = ConvertToPropertyAndAddress(reader);
+
+                    if(properties.Count == 1)
+                    {
+                        property = properties[0];
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
             return property;
         }
 
+        private List<PropertyAndAddress> ConvertToPropertyAndAddress(SqlDataReader reader)
+        {
+            List<PropertyAndAddress> properties = new List<PropertyAndAddress>();
+            while (reader.Read())
+            {
+                PropertyAndAddress prop = new PropertyAndAddress();
 
+                prop.propertyId = (int)reader["property_id"];
+                prop.addressId = (int)reader["address_id"];
+                prop.userId = (int)reader["userId"];
+                prop.Bedrooms = (int)reader["bedrooms"];
+                prop.Bathrooms = (int)reader["bathrooms"];
+                prop.Photo = (string)reader["photo"];
+                prop.Description = (string)reader["prop_desc"];
+                prop.Price = (decimal)reader["price"];
+                prop.Property_Type = (string)reader["property_type"];
+                prop.Street = (string)reader["street"];
+                prop.city = (string)reader["city"];
+                prop.region = (string)reader["region"];
+                prop.zip = (int)reader["zip"];
+                if ((string)reader["street2"] != "N/A")
+                {
+                    prop.Street2 = (string)reader["street2"];
+                }
+                else
+                {
+                    prop.Street2 = "";
+                }
+
+                properties.Add(prop);
+            }
+            return properties;
+        }
     }
 }
