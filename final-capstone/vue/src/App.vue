7dd2c2ca331ee3dbf2ea6f8c12ce4ac31b4a54f0
Braydon Griffith <braydon0898@gmail.com>
2020-12-08 12:45:53 -0500

    updated parallax page and property listing

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <v-app>
+    <v-parallax src="@/assets/Background.jpg"> </v-parallax>
     <nav-bar />
     <v-main>
       <router-view />
     </v-main>
     <page-footer />
-    
-
   </v-app>
 </template>
 
 <script>
-import PageFooter from './components/PageFooter.vue';
-import NavBar from './components/NavBar.vue';
+import PageFooter from "./components/PageFooter.vue";
+import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
 
   components: {
     NavBar,
-    PageFooter
-    
+    PageFooter,
   },
 
   data: () => ({
@@ -28,4 +26,3 @@ export default {
   }),
 };
 </script>
-
