6d3e219d05dc7dd8cee939e062d02cc7f61823d5
jacob barnett <jabarnett97@gmail.com>
2020-12-09 11:27:05 -0500

    Refactored merge conflicts

--- final-capstone/dotnet/Capstone/Models/Lease.cs
+++ final-capstone/dotnet/Capstone/Models/Lease.cs
@@ -41,12 +41,6 @@ namespace Capstone.Models
         public DateTime Todate { get; set; }
         public int Userid { get; set; }
         public int Propertyid { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Phonenumber { get; set; }
-        public string Email { get; set; }
-        public string Dob { get; set; }
-        public bool isapproved { get; set; }
 
 
 
