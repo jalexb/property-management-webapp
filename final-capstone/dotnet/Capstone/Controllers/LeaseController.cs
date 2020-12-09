8e0c08d3f64a61ac2df2b4a2a4a8eb6a3676902d
jacob barnett <jabarnett97@gmail.com>
2020-12-09 11:06:28 -0500

    addPendingLease function

--- final-capstone/dotnet/Capstone/Controllers/LeaseController.cs
+++ final-capstone/dotnet/Capstone/Controllers/LeaseController.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.DAO;
+using Capstone.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Controllers
 {
+
+    [Route("[controller]")]
+    [ApiController]
     public class LeaseController : Controller
     {
-        
+
+        private readonly ILeaseDAO leaseDAO;
+        public LeaseController(ILeaseDAO _leaseDAO)
+        {
+            leaseDAO = _leaseDAO;
+        }
+
+
+        [HttpPost("{pending_lease}")] //localhost:43545/lease/{pending_lease_object}
+        public IActionResult addPendingLease(PendingLease lease)
+        {
+            int rowCount = leaseDAO.AddPendingLease(lease);
+
+            if(rowCount == 1)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+             
     }
 }
