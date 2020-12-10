a9908d6876d1fe9514a9f5a4c47f84e251161beb
Amy Scott <amy.ack.scott@gmail.com>
2020-12-09 19:26:25 -0500

    updated login form styles removed Lease Application from Nav-bar

--- final-capstone/vue/src/views/Login.vue
+++ final-capstone/vue/src/views/Login.vue
@@ -12,27 +12,33 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
+      <div class="form-group">
+          <label for="username" class="sr-only">Username</label>
+            <input
+              type="text"
+              id="username"
+              class="form-control"
+              placeholder="Username"
+              v-model="user.username"
+              required
+              autofocus
+            />
+      </div>
+      <div class="form-group">
+         <label for="password" class="sr-only">Password</label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+      </div>
+      <div class="form-group">
+          <router-link :to="{ name: 'register' }">Need an account?</router-link>
+          <button type="submit">Sign in</button>
+      </div>
     </form>
   </div>
 </template>
@@ -74,3 +80,21 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.form-group {
+  padding: 20px;
+}
+label {
+  padding: 20px;
+  font-weight: bold;
+}
+button {
+  padding: 20px;
+  font-weight: bold;
+}
+</style>
