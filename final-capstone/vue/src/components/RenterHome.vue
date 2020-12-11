7558775ee662bb9efe9183bbf140db3ee6ca6098
Evan Gemmer <evangemmer@gmail.com>
2020-12-11 14:02:27 -0500

    Fixed formatting of cards on renter home page so that the cards are the same size and the text is all on one line

--- final-capstone/vue/src/components/RenterHome.vue
+++ final-capstone/vue/src/components/RenterHome.vue
@@ -10,10 +10,10 @@
             >
               <v-card
               class="ma-6"
-              max-width="300"
+              max-width="350"
               hover>
               <!--<router-link :to="{ name: 'property-details', params: {id: property.propertyId} }">-->
-              <v-img src="https://www.investopedia.com/thmb/M-we6mdLUgezNP-XsAoli0Lf070=/2121x1414/filters:fill(auto,1)/GettyImages-974812152-705d0a6d517947fb8946a3c887d528be.jpg"> </v-img>
+              <v-img max-height="200" src="https://www.investopedia.com/thmb/M-we6mdLUgezNP-XsAoli0Lf070=/2121x1414/filters:fill(auto,1)/GettyImages-974812152-705d0a6d517947fb8946a3c887d528be.jpg"> </v-img>
               <v-card-title> 
                 Pay Rent
               </v-card-title>
@@ -22,13 +22,12 @@
             <router-link :to="{name: 'maintenance-ticket', params: {id: this.$store.state.user.userId}}">
               <v-card 
                 class="ma-6"
-                max-width="300"
+                max-width="350"
                 hover>
-                <!--<router-link :to="{ name: 'property-details', params: {id: property.propertyId} }">-->
-                <v-img src="https://media.noria.com/sites/Uploads/2019/1/29/6043ff94-3285-4abc-b169-6ab2793a0c24_Strategies%20to%20Reduce%20Maintenance%20Costs_extra_large.jpeg"/>          <v-card-title> 
+                <v-img max-height="200" src="https://media.noria.com/sites/Uploads/2019/1/29/6043ff94-3285-4abc-b169-6ab2793a0c24_Strategies%20to%20Reduce%20Maintenance%20Costs_extra_large.jpeg"/>          
+                <v-card-title> 
                   Submit a Maintenance Request
                 </v-card-title>
-                <!--</router-link>-->
               </v-card>
             </router-link>
             </v-flex>
