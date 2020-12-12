875afb1a432a9f8d5baa69362486b62756fb918f
jacob barnett <jabarnett97@gmail.com>
2020-12-11 22:39:54 -0500

    API request should take the lease_id, not the user_id

--- final-capstone/vue/src/components/PendingLeases.vue
+++ final-capstone/vue/src/components/PendingLeases.vue
@@ -1,15 +1,98 @@
 <template>
-<div>
+<div style="width: 80%; margin-left: 10%; text-align: center;">
   <h1>Pending Leases</h1>
+  <v-flex>
+  <v-card >
+    <v-card-text>
+      <table style="width: 100%">
+        <tr>
+          <th>Applicant Name</th>
+          <th>Email</th>
+          <th>Phone Number</th>
+          <th>From Date</th>
+          <th>To Date</th>
+          <th>Location</th>
+          <th>Approve / Reject Application</th>
+        </tr>
+        <tr
+        v-for="pendingLease in pendingLeases" 
+        :key="pendingLease.Lease_Id"
+        :property_id="pendingLease.Property_Id"
+        hover>
+          <td>{{pendingLease.User_Info.fullName}}</td>
+          <td>{{pendingLease.User_Info.email}}</td>
+          <td>{{pendingLease.User_Info.phoneNumber}}</td>
+          <td>{{pendingLease.From_Date}}</td>
+          <td>{{pendingLease.To_Date}}</td>
+          <td>{{pendingLease.User_Info.address}}</td>
+          <td>
+            <v-button v-on:click="approveLease(pendingLease.Lease_Id)" class="approve"> Approve </v-button> &nbsp;
+            <v-button v-on:click="rejectLease(pendingLease.Lease_Id)" class="reject"> Reject </v-button>
+          </td>
+        </tr>
+      </table>
+    </v-card-text>
+    
+  </v-card>
+  </v-flex>
 </div>
 </template>
 
 <script>
-export default {
+import LandlordService from '@/services/LandlordService';
 
+export default {
+  name: 'pending-leases',
+  data() {
+    return {
+      pendingLeases:[{
+          Lease_Id: null, 
+          From_Date: null, 
+          To_Date: null, 
+          User_Id: null,
+          userInfo: /*{address, email, fullName, phoneNumber, property_Id, user_Id}*/ null,
+          Property_Id: null,
+          }],
+      userId: this.$store.state.userId,
+      
+    }
+  },
+  created() {
+    //returns and object with pendingLeases with that lease's userInformation.
+      this.pendingLeases = LandlordService.getPendingLeases();
+  },
+  methods: {
+    //get renter information
+    
+    //approve lease
+    approveLease(leaseId) {
+      LandlordService.approveLease(leaseId);
+    },
+    //reject lease
+    rejectLease(leaseId) {
+      LandlordService.rejectLease(leaseId);
+    }
+  }
 }
 </script>
 
 <style>
+
+th {
+  border-style: outset;
+}
+button {
+  padding: 0px 10px;
+}
+
+.approve:hover {
+  background-color: green;
+  cursor: pointer;
+}
+
+.reject:hover{
+  background-color: red;
+  cursor: pointer;
+}
 
 </style>
--- final-capstone/vue/src/services/LandlordService.js
+++ final-capstone/vue/src/services/LandlordService.js
@@ -5,5 +5,61 @@ const http = axios.create({
   });
 
 export default {
-    
+    //get pending leases
+    getPendingLeases() {
+      //http.get('/pendingLeases/' + userId)
+      return [
+        {
+          Lease_Id: 2, 
+          From_Date: '2020-12-17', 
+          To_Date: '2021-12-17', 
+          User_Id: 6, 
+          Property_Id: 11,
+          User_Info: {
+            address:"37942 Eagle DrGatlinburgTennessee37738",
+            email:"j",
+            fullName:"f j",
+            phoneNumber:"j",
+            property_Id:11,
+            user_Id:6,
+          }
+        },
+        {
+          Lease_Id: 3, 
+          From_Date: '2020-12-17', 
+          To_Date: '2021-12-17', 
+          User_Id: 6, 
+          Property_Id: 12,
+          User_Info: {
+            address:"37942 Eagle DrGatlinburgTennessee37738",
+            email:"j",
+            fullName:"f j",
+            phoneNumber:"j",
+            property_Id:11,
+            user_Id:6,
+          }
+        },
+        {
+          Lease_Id: 4, 
+          From_Date: '2020-12-17', 
+          To_Date: '2021-12-17', 
+          User_Id: 6, 
+          Property_Id: 13,
+          User_Info: {
+            address:"37942 Eagle DrGatlinburgTennessee37738",
+            email:"j",
+            fullName:"f j",
+            phoneNumber:"j",
+            property_Id:11,
+            user_Id:6,
+          }
+        }
+      ]
+    },
+    approveLease(leaseId) {
+      return http.post('lease/approve/' + leaseId);
+    },
+    rejectLease(leaseId) {
+      return http.post('lease/reject/' + leaseId);
+    }
 }
