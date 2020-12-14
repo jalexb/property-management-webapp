0f7bad11b815e58576a25f05e8f00dce1ff7b872
Braydon Griffith <braydon0898@gmail.com>
2020-12-14 10:58:19 -0500

    Created new vuetify register page and updated functionality

--- final-capstone/vue/src/views/Login.vue
+++ final-capstone/vue/src/views/Login.vue
@@ -41,53 +41,14 @@
                   required
                 ></v-text-field>
                 <v-btn type="submit" color="secondary">Login</v-btn>
+                <v-spacer></v-spacer>
+                <router-link :to="{ name: 'register' }">Need an account?</router-link>
               </v-form>
             </v-card-text>
           </v-card>
         </v-flex>
       </v-layout>
     </v-container>
-
-    <!-- <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <div class="form-group">
-          <label for="username" class="sr-only">Username</label>
-            <input
-              type="text"
-              id="username"
-              class="form-control"
-              placeholder="Username"
-              v-model="user.username"
-              required
-              autofocus
-            />
-      </div>
-      <div class="form-group">
-         <label for="password" class="sr-only">Password</label>
-          <input
-            type="password"
-            id="password"
-            class="form-control"
-            placeholder="Password"
-            v-model="user.password"
-            required
-          />
-      </div>
-      <div class="form-group">
-          <router-link :to="{ name: 'register' }">Need an account?</router-link>
-          <button type="submit">Sign in</button>
-      </div>
-    </form> -->
   </div>
 </template>
 
