02035df1dec5316912b931bc53ac55d32a33b1c0
Evan Gemmer <evangemmer@gmail.com>
2020-12-17 11:51:35 -0500

    Added a Google font that looks more like the RentR logo

--- final-capstone/vue/src/components/RenterHome.vue
+++ final-capstone/vue/src/components/RenterHome.vue
@@ -11,7 +11,7 @@
             <router-link :to="{name: 'renter-transactions', params: {id: this.$store.state.user.userId}}">
               <v-card
               class="ma-6"
-              max-width="350"
+              max-width="360"
               hover>
               <v-img max-height="200" src="https://www.investopedia.com/thmb/M-we6mdLUgezNP-XsAoli0Lf070=/2121x1414/filters:fill(auto,1)/GettyImages-974812152-705d0a6d517947fb8946a3c887d528be.jpg"> </v-img>
               <v-card-title> 
@@ -22,7 +22,7 @@
             <router-link :to="{name: 'maintenance-ticket', params: {id: this.$store.state.user.userId}}">
               <v-card 
                 class="ma-6"
-                max-width="350"
+                max-width="360"
                 hover>
                 <v-img max-height="200" src="https://media.noria.com/sites/Uploads/2019/1/29/6043ff94-3285-4abc-b169-6ab2793a0c24_Strategies%20to%20Reduce%20Maintenance%20Costs_extra_large.jpeg"/>          
                 <v-card-title> 
@@ -33,7 +33,7 @@
              <router-link :to="{name: 'renter-settings'}">
               <v-card
               class="ma-6"
-              max-width="350"
+              max-width="360"
               hover>
               <v-img max-height="200" src="https://media.istockphoto.com/vectors/gear-icon-outline-icon-business-services-technical-help-gear-support-vector-id1024061294?k=6&m=1024061294&s=612x612&w=0&h=jbvUYCFO0v99ciwU461v-YqZBnGMkDG6li6KPW09m5s="> </v-img>
               <v-card-title> 
