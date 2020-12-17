df7c9fe3b4624222c5d3f76b60b443690ba8f3cd
Amy Scott <amy.ack.scott@gmail.com>
2020-12-17 17:22:00 -0500

    updates the sql for the maintenance request

--- final-capstone/dotnet/database/capstone.sql
+++ final-capstone/dotnet/database/capstone.sql
@@ -308,10 +308,10 @@ VALUES (1, 2, '03-01-2021', 0, 0, 0);
 
 --populate default maintenance ticket data
 INSERT INTO maintenance_request (renter_id, worker_id, request_info, property_id, is_assigned, is_fixed, post_fix_report)
-VALUES(3, 4, 'The shower only gets cold water.', 8, 1, 0, '');
+VALUES(3, null, 'The shower only gets cold water.', 8, 1, 0, '');
 
 INSERT INTO maintenance_request (renter_id, worker_id, request_info, property_id, is_assigned, is_fixed, post_fix_report)
-VALUES(3, 4, 'The garbage disposal for the kitchen sink doesn''t work.', 9, 1, 0, '');
+VALUES(3, null, 'The garbage disposal for the kitchen sink doesn''t work.', 9, 1, 0, '');
 
 INSERT INTO maintenance_request (renter_id, worker_id, request_info, property_id, is_assigned, is_fixed, post_fix_report)
 VALUES(3, null, 'My basement flooded', 9, 1, 0, '');
